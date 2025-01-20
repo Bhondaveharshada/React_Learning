8db00ae7a4c442390e5a3fad76873643f057b5e1
Harshada Bhondave <manishabhondave640@gmail.com>
2025-01-20 23:28:56 +0530

    learned inline event handler ,passing args to Event handler

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -8,14 +8,31 @@ export const EventHandling =()=>{
         
         
         alert("this is click event")
+    };
+   
+    const handleWelocmeUser = (user)=>{
+        console.log(`hey ${user} welcome to this react project`);
+        
     }
+
     return <>
     {/* { 1. normal function calling on onclick event} */}
        <button onClick={handleClickEvent}> Click Me</button>
        <br />
        <br />
        {/* { 2. fat arrow function calling on onclick event} */}
-       <button onClick={()=> handleClickEvent(event)}>Click Me 2</button>  
+       <button onClick={()=> handleClickEvent(event)}>Click Me 2</button>
+       <br /> 
+       <br />
+       {/* inline event handler and inline arrow funtion */}
+       <button onClick={(event)=>console.log(event)}>inline function</button> 
+       <br /><br />
+       <button onClick={()=>alert("Hey I am inline arrow funtion")}>inline Arr function</button> 
+       <br /><br />
+       {/* passing arguments to Event handler */}
+       <button onClick={() =>handleWelocmeUser("Harshada")}>Click Me 3</button>
+       
+
     </>
 
 }
